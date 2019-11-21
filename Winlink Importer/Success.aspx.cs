f9f9fe6bb8d7a305890c0541cb67f0f2a636e919
kd5sko <kd5sko@gmail.com>
2019-11-21 11:54:54 -0600

    update success page

--- Winlink Importer/Success.aspx.cs
+++ Winlink Importer/Success.aspx.cs
@@ -11,7 +11,7 @@ namespace Winlink_Importer
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            result.InnerHtml = Session["Message"].ToString();
         }
     }
 }
